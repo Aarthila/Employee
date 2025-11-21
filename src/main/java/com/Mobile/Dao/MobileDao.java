b431e39a966a90450985aaed9c5c70f90e2e4d5c
AarthigaElavarasan <aarthigaelavarasan16@gmail.com>
2025-11-21 12:39:41 +0530

    Update Mobile

--- src/main/java/com/Mobile/Dao/MobileDao.java
+++ src/main/java/com/Mobile/Dao/MobileDao.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
 import com.Mobile.Repository.MobileRepository;
 import com.Mobile.entity.MobileEntity;
 
